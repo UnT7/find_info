b47b10c2560e56ec4c014945973693448c9b6db2
hackerxj007 <320532704@qq.com>
2024-04-14 21:30:18 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module find_info
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"find_info/find"
+	"find_info/info"
+)
+
+func main() {
+
+	info.Main()
+
+	find.Findfile()
+
+	//print("111")
+
+}
